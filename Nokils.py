92799473ec327dc2a2bf6e21405a9029e608909f
llllllllllg <layth.dabit08@gmail.com>
2024-05-07 14:27:00 +0200

    Add files via upload

---
+++ Nokils.py
@@ -0,0 +1,10 @@
+# Nokils.py
+
+def add_numbers(a, b):
+    """
+    هذه الدالة تقوم بجمع اثنين من الأعداد
+    :param a: العدد الأول
+    :param b: العدد الثاني
+    :return: ناتج جمع الأعداد
+    """
+    return a + b
